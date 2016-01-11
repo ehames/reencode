a788291c8473800ba0868afad8858c8186387f67
Edgardo Hames <ehames@gmail.com>
2016-01-11 20:54:33 -0300

    Updated doc

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 This bash script reencodes movies so they can be played on DVD players
 including Philips PD 9016/37 (portable DVR player for cars).
 
-Usage: reencode <input file> <title>
-A new avi file is created, input file is not modified.
+Usage: reencode <input file>
+A new avi file is created in "reencode" directory (input file is not modified).
 
 Author: Edgardo Hames - ehames@gmail.com
--- reencode.sh
+++ reencode.sh
@@ -19,9 +19,9 @@
 # This bash script reencodes movies so they can be played on DVD players
 # including Philips PD 9016/37 (portable DVR player for cars).
 #
-# Usage: reencode <input file> <title>
+# Usage: reencode <input file>
 #
-# A new avi file is created, input file is not modified.
+# A new avi file is created in "reencode" directory (input file is not modified).
 
 
 OUTPATH="./reencode"
